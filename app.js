471e65fcf594874ac763869d0eca8ee7a8547a54
Aldino Pratama <aldinopratama15@gmail.com>
2021-06-27 23:36:11 +0700

    Set middleware global

--- app.js
+++ app.js
@@ -15,18 +15,27 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
 // Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(logger("dev"));
 }
+// Limit requests from same API
 const limiter = rateLimit({
     max: 5,
     windowMs: 05 * 60 * 1000,
     message: "Too many requests from this IP, please try again in an hour!",
   });
-  app.use("/api", limiter);
-
+app.use("/api", limiter);
+// Body parser, reading data from body into req.body
 app.use(express.json());
+// app.use(express.json({ limit: "10kb" }));
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xss());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
