937af2e008420aec31b3d6f7db2c47e7492f153e
Aldino Pratama <aldinopratama15@gmail.com>
2021-06-27 23:26:46 +0700

    access limit

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -13,6 +19,13 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(logger("dev"));
 }
+const limiter = rateLimit({
+    max: 5,
+    windowMs: 05 * 60 * 1000,
+    message: "Too many requests from this IP, please try again in an hour!",
+  });
+  app.use("/api", limiter);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
