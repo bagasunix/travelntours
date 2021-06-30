1075b58e590293ecfff0c427b70b5da21ae1c50b
Aldino Pratama <aldinopratama15@gmail.com>
2021-06-30 15:19:19 +0700

    install slugify

--- controllers/tourC.js
+++ controllers/tourC.js
@@ -1,3 +1,8 @@
+// Lib Validator
+const Validator = require('fastest-validator');
+
+const v = new Validator();
+// /Model
 const Tour = require('../models/tourM');
 
 module.exports = {
@@ -18,12 +23,120 @@ module.exports = {
     }
   },
   create: async (req, res, next) => {
-    console.log('Hallo ini create');
+    try {
+      // const userId = await User.findById(req.user.id);
+
+      const {
+        name,
+        duration,
+        maxGroup,
+        difficulty,
+        ratingsAverage,
+        ratingsQuantity,
+        price,
+        summary,
+        description,
+        imageCover,
+        images,
+        startDates,
+        startLocation,
+        locations,
+        guides,
+      } = req.body;
+
+      const schema = {
+        name: 'string|empty:false',
+        duration: 'number|empty:false',
+        maxGroup: 'number|empty:false',
+        difficulty: 'string|empty:false',
+        ratingsAverage: 'number|empty:false',
+        ratingsQuantity: 'number|empty:false',
+        price: 'number|empty:false',
+        summary: 'string|empty:false',
+        description: 'string|empty:false',
+        imageCover: 'string|empty:false',
+        images: 'array|item:string|empty:false',
+        startDates: 'array|item:date|empty:false',
+        startLocation: {
+          type: 'object',
+          strict: true,
+          props: {
+            type: {
+              type: 'string',
+              empty: false,
+              enum: ['Point'],
+              default: 'Point',
+            },
+            coordinates: { type: 'array', empty: false, items: 'number' },
+            address: 'string|empty:false',
+            description: 'string|empty:false',
+          },
+        },
+        locations: {
+          type: 'array',
+          strict: true,
+          props: {
+            type: {
+              type: 'string',
+              empty: false,
+              enum: ['point'],
+              default: 'point',
+            },
+            coordinates: { type: 'array', empty: false, items: 'number' },
+            address: 'string|empty:false',
+            description: 'string|empty:false',
+            day: 'number|empty:false',
+          },
+        },
+        guides: {
+          type: 'array',
+          items: 'string',
+          empty: false,
+        },
+      };
+
+      const checkName = await Tour.findOne({ name: name });
+
+      if (checkName) {
+        return res.status(400).json({
+          status: 'error',
+          message: 'Name tour is already exist',
+        });
+      }
+
+      const newTour = await Tour.create({
+        name,
+        duration,
+        maxGroupSize: maxGroup,
+        difficulty,
+        ratingsAverage,
+        ratingsQuantity,
+        price,
+        summary,
+        description,
+        imageCover,
+        images,
+        startDates,
+        startLocation,
+        locations,
+        guides,
+      });
+
+      res.status(201).json({
+        status: 'success',
+        data: newTour,
+      });
+    } catch (err) {
+      return res.status(res.status).json({
+        status: res.status,
+        message: res.message,
+      });
+    }
   },
   update: async (req, res, next) => {
-    console.log('Hallo ini update');
+    // console.log('Hallo ini update');
   },
   delete: async (req, res, next) => {
-    console.log('Hallo ini delete');
+    // console.log('Hallo ini delete');
   },
 };
--- models/tourM.js
+++ models/tourM.js
@@ -1,53 +1,19 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'A tour must have a name'],
-      unique: true,
-      trim: true,
-      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
-      minlength: [10, 'A tour name must have more or equal then 10 characters'],
-    },
-    duration: {
-      type: Number,
-      required: [true, 'A tour must have a duration'],
-    },
-    ratingsAverage: {
-      type: Number,
-      default: 4.5,
-      min: [1, 'Rating must be above 1.0'],
-      max: [5, 'Rating must be below 5.0'],
-    },
-    ratingsQuantity: {
-      type: Number,
-      default: 0,
-    },
-    price: {
-      type: Number,
-      required: [true, 'A tour must have a price'],
-    },
-    summary: {
-      type: String,
-      trim: true,
-      required: [true, 'A tour must have a description'],
-    },
-    description: {
-      type: String,
-      trim: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now(),
-      select: false,
-    },
-    startDates: Date,
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
   },
-  {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-  }
-);
+  rating: {
+    type: Number,
+    default: 3.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
 
 module.exports = mongoose.model('Tour', tourSchema);
